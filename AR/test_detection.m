50a4731046b4b67cf7ebb7917088a0460527d4f6
Alessandro Erba <alessandro.erba@cispa.saarland>
2022-09-16 17:15:39 +0200

    fix paths

--- AR/test_detection.m
+++ AR/test_detection.m
@@ -7,7 +7,7 @@ Test_1 = readtable('../Spoofing Framework/BATADAL/test_dataset_1_datetime.csv');
 
 Attack_stale_1 = readtable('../Spoofing Framework/BATADAL/unconstrained_spoofing/test_dataset_1_stale.csv');
 Attack_random_replay_1 = readtable('../Spoofing Framework/BATADAL/unconstrained_spoofing/test_dataset_1_random_replay.csv');
-Attack_acsac = readtable('../BATADAL_BLACK_BOX_ATTACKS_ACSAC/unconstrained_attack/test_dataset_1_unconstrained_newAE.csv');
+Attack_acsac = readtable('../BATADAL_BLACK_BOX_ATTACKS_ACSAC/unconstrained_spoofing/test_dataset_1_unconstrained_newAE.csv');
 
 disp('Test');
 [accuracy_1, precision_1, recall_1, f1_1, fpr_1] = AR_detection(15, 5.5, 1, Train, Test_1);
